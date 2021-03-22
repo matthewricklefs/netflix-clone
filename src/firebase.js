39ca818d7a84315e0624cb65a51b419977bac2c1
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-22 12:53:19 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkNnLrAUtNbT8RuvYoFioLasyXRM7CBDA",
+  authDomain: "netflix-clone-a4e0c.firebaseapp.com",
+  projectId: "netflix-clone-a4e0c",
+  storageBucket: "netflix-clone-a4e0c.appspot.com",
+  messagingSenderId: "665998474707",
+  appId: "1:665998474707:web:8c0d1c6bfb67fee4908326"
+};
