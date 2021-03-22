ea3c7c4dc5ddf0cee21c3edd45c015af28a2a9e3
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-22 13:54:33 -0400

    creates Banner.js and Banner.css, updates global styles

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,38 +1,14 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import Banner from "./Banner";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
 
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      <h1> TEST </h1>
-      
-      {/* Banner */}
+      <Banner />
 
       {/* Row */}
     </div>
