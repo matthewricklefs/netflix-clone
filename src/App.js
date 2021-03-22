d3b507f9fe1e013e07757d595ebcd37cda950046
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-22 13:35:55 -0400

    creates Nav.js and Nav.css, imports logo and avatar images to Nav.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import HomeScreen from "./HomeScreen";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build Netflix</h1>
+      <HomeScreen />
     </div>
   );
 }
