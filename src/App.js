ee1ad43dd534bfa87c557993a7d0f2b8de2ca5d1
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-23 14:43:17 -0400

    adds useEffect and authentication from firebase to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from "react";
-import HomeScreen from "./screens/HomeScreen";
-import Login from "./screens/LoginScreen";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import React, { useEffect } from "react";
 import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth);
+      } else {
+        // Logged out
+      }
+    });
+
+    return unsbuscribe;
+  }, []);
 
   return (
     <div className="app">
       <Router>
         {!user ? (
-          <Login />
+          <LoginScreen />
         ) : (
           <Switch>
             <Route path="/">
