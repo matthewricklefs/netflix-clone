705f697c461b859d6785676b080062512076440c
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-23 16:04:25 -0400

    implements cross application navigation using react router, fixes dispatch error with authenticating and remembering users logged in

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        // Logged in
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         // Logged out
+        dispatch(logout);
       }
     });
 
@@ -28,6 +37,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route path="/">
               <HomeScreen />
             </Route>
