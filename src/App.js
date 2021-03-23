0d29d1e45ca977774af16f415dc023ca656e0987
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-23 12:42:30 -0400

    restructures file directory by adding screens directory, begins to layout the LoginScreen component to prepare for authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import Login from "./screens/LoginScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <Switch>
+            <Route path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
