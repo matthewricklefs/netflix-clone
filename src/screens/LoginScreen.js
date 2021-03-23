c65d6bc922979ec307968dac2719438a9ecafb0d
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-23 13:37:22 -0400

    creates the styles for the LoginScreen component

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -17,6 +17,19 @@ function LoginScreen() {
         <div className="loginScreen__body">
           <>
             <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+
+                <button className="loginScreen__getStarted">GET STARTED</button>
+              </form>
+            </div>
           </>
         </div>
       </div>
